292374bfab1536b6af2bd79d232aafb3c77359f4
Jrperez79 <joelrperez@gmail.com>
2020-08-10 08:22:10 -0700

    updated because of failed deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link
 } from 'react-router-dom';
 import SearchPage from './SearchPage/SearchPage.js';
 import DetailPage from './DetailPage/DetailPage.js';
